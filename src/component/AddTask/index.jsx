e9de21193ebac2c831f32c5e488e1b5d309b477b
Axel Hégron <axel.hegron@3wa.io>
2023-05-26 13:37:33 +0200

    update in add feature

--- src/component/AddTask/index.jsx
+++ src/component/AddTask/index.jsx
@@ -1,13 +1,25 @@
 import { useState } from "react";
-import { addTask } from "../../store/actions/taskActions";
+// import { addTask } from "../../store/actions/taskActions";
 
-function AddTask() {
-  const [task, setTask] = useState("");
+function AddTask({ onSubmit }) {
+  const [task, setTask] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(task);
+    setTask('')
+  }
 
   return (
     <div>
-      <form>
-        <input type={"text"} placeholder={"Nom de votre tache"} />
+      <form onSubmit={handleSubmit}>
+        <input
+          type={"text"}
+          placeholder={"Nom de votre tache"}
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button role={'submit'}>Ajouter</button>
       </form>
     </div>
   );
--- src/pages/TaskFormPage.jsx
+++ src/pages/TaskFormPage.jsx
@@ -1,9 +1,18 @@
+
+import {useDispatch, useSelector} from "react-redux";
+import {addTask, deleteTask} from "../store/actions/taskActions.js"
+import { TaskSelector } from "../store/selectors/taskSelector.js";
 import AddTask from "../component/AddTask/index"
 
+
 function TaskFormPage() {
+  const {tasks} = useSelector(TaskSelector)
+  const dispatch = useDispatch();
+  const onAddTask = (name) => dispatch(addTask(name))
+
   return (
     <div>
-      <AddTask/>
+      <AddTask onSubmit={onAddTask}/>
     </div>
   )
 }
